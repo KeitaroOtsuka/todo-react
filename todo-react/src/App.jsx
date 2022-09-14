6cb5005a35ad9eb15d48aac444dfb493b76f185a
KeitaroOtsuka <ktrotsuka@gmail.com>
2022-09-14 23:11:46 +0900

    useStateを用いた記述方法に変更

--- todo-react/src/App.jsx
+++ todo-react/src/App.jsx
@@ -1,4 +1,14 @@
+import { useState } from "react";
+
 export const App = () => {
+  const [incompleteTodos, setIncompleteTodos] = useState([
+    'ああああ',
+    'いいいい'
+  ]);
+  const [completeTodos, setCompleteTodos] = useState([
+    'うううう',
+    'ええええ'
+  ])
   return (
     <>
       <div className="input-area">
@@ -8,29 +18,28 @@ export const App = () => {
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
         <ul>
-          <div className="list-row">
-            <li>ああああ</li>
-            <button>完了</button>
-            <button>削除</button>
-          </div>
-          <div className="list-row">
-            <li>いいいい</li>
-            <button>完了</button>
-            <button>削除</button>
-          </div>
+          {incompleteTodos.map((todo) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>完了</button>
+                <button>削除</button>
+              </div>
+            )
+          })}
         </ul>
       </div>
       <div className="complete-area">
       <p className="title">完了のTODO</p>
         <ul>
-          <div className="list-row">
-            <li>ああああ</li>
-            <button>戻す</button>
-          </div>
-          <div className="list-row">
-            <li>いいいい</li>
-            <button>戻す</button>
-          </div>
+          {completeTodos.map((todo) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>戻す</button>
+              </div>
+            );
+          })}
         </ul>
       </div>
     </>
