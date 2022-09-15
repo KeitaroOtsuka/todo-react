3b56232c0bddd3a1e76702387bdf0edf79be1383
KeitaroOtsuka <ktrotsuka@gmail.com>
2022-09-15 18:16:42 +0900

    完了ボタン実装

--- todo-react/src/App.jsx
+++ todo-react/src/App.jsx
@@ -24,6 +24,15 @@ export const App = () => {
     newTodos.splice(index, 1);
     setIncompleteTodos(newTodos);
   };
+  const onClickComplete = (index) => {
+    const newIncompleteTodos = [...incompleteTodos];
+    newIncompleteTodos.splice(index, 1);
+
+    const newCompleteTodos = [...completeTodos, incompleteTodos[index]];
+
+    setIncompleteTodos(newIncompleteTodos);
+    setCompleteTodos(newCompleteTodos);
+  }
   return (
     <>
       <div className="input-area">
@@ -37,7 +46,7 @@ export const App = () => {
             return (
               <div key={todo} className="list-row">
                 <li>{todo}</li>
-                <button>完了</button>
+                <button onClick={() => onClickComplete(index)}>完了</button>
                 <button onClick={() => onClickDelete(index)}>削除</button>
               </div>
             )// 関数に引数を渡すとその時点で関数内が実行されるため、アロー関数で定義することで任意のタイミングで関数を実行できる。
